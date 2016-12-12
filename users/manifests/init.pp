b93d10b66ed16de08019900824530e2f715a98c7
jaakkojokinen <jaakkojokinen@users.noreply.github.com>
2016-12-12 17:23:41 +0200

    Update init.pp

--- users/manifests/init.pp
+++ users/manifests/init.pp
@@ -2,7 +2,7 @@ class users {
   user { 'jaakko':
 		comment => 'Jaakko Jokinen',
 		ensure => 'present',
-		password => '$1$j.zjP0iG$RaopTvU99StlZCyFq/oE51',
+		password => '',
 		managehome => true,
   }
 
